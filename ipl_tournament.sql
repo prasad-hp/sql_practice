5584bdfcd0afcd02a786ef4e5d2f1311f48ce5e9
prasad-hp <itsprasadhp@gmail.com>
2025-05-09 00:39:49 +0530

    Update ipl_tournament.sql

--- ipl_tournament.sql
+++ ipl_tournament.sql
@@ -10,5 +10,16 @@ ROW_NUMBER() OVER() AS SlNo
 SELECT a1.teams AS A1_Team, a2.teams AS A2_Team FROM add_index a1
 JOIN add_index a2
 ON a1.slno > a2.slno
+-- 45 Maches in total for 10 teams
 
 -- two matches with each team
+WITH add_index AS (
+SELECT 
+ROW_NUMBER() OVER() AS SlNo
+, * FROM ipl
+)
+
+SELECT a1.teams AS A1_Team, a2.teams AS A2_Team FROM add_index a1
+JOIN add_index a2
+ON a1.slno != a2.slno
+-- 90 Maches in total for 10 teams
